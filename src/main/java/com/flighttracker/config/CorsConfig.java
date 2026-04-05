adec15279592e0d41f96d1a514533ed90a337765
TusharSamaniya <tushasamaniya4956@gmail.com>
2026-04-05 14:16:16 +0530

    Allows your React app to talk to Spring Boot

---
+++ src/main/java/com/flighttracker/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.flighttracker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+	
+	public WebMvcConfigurer corsConfiguration() {
+		
+		return new WebMvcConfigurer() {
+			
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*");
+			}
+		};
+		
+	}
+
+}
